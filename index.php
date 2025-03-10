7b00d816aaee885c099977005f62d878d5f5746b
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-10 11:35:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -203,9 +203,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
   </script>
   <!-- Github buttons -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- <script async defer src="https://buttons.github.io/buttons.js"></script> -->
   <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="assets/js/material-dashboard.min.js?v=3.2.0"></script>
+  <!-- <script src="assets/js/material-dashboard.min.js?v=3.2.0"></script> -->
 </body>
 
 </html>
