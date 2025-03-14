8cb63a9164ac72b9a6eb94e01448ebe2e64f0abd
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-15 05:11:06 +0530

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,9 +1,9 @@
 <?php
 // Database Connection
 $servername = "localhost";
-$username = "u820431346_account";
-$password = "o0V5DnDLqbR*";
-$dbname = "u820431346_account";
+$username = "u902379465_fitness";
+$password = "m|ETEq6jU0";
+$dbname = "u902379465_fitness";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
 ?>
