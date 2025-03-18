deb673d4430a35e2d39937f424f7f046ae2d1e5b
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-18 12:34:57 +0530

    Update all-patient.php

--- all-patient.php
+++ all-patient.php
@@ -61,7 +61,7 @@
           <div class="container-fluid">
             <!--begin::Row-->
             <div class="row">
-              <div class="col-md-6">
+              <div class="col-md-12">
                 <div class="card mb-4">
                   <div class="card-header"><h3 class="card-title">Bordered Table</h3></div>
                   <!-- /.card-header -->
