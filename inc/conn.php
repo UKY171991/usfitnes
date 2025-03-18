a5d92ca472c9362ed9e22b920c97ebd7f474d751
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-18 12:33:10 +0530

    Update conn.php

--- inc/conn.php
+++ inc/conn.php
@@ -1,1 +1,14 @@
-conn.php
+<?php
+$servername = "localhost";   // Replace if using another host
+$username = "u902379465_fitness";          // Replace with your DB username
+$password = "^ytIkNdD=1K";              // Replace with your DB password
+$dbname = "u902379465_fitness";    // Replace with your database name
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
