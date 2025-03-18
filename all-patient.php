c19e6cc95c50f488a016fb3e171dcc1761bcb3bd
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-18 13:49:52 +0530

    Update all-patient.php

--- all-patient.php
+++ all-patient.php
@@ -60,7 +60,7 @@
             <div class="row">
               <div class="col-md-12">
                 <div class="card mb-4">
-                  <div class="card-header"><h3 class="card-title">Bordered Table</h3></div>
+                  <div class="card-header"><h3 class="card-title">Patient List</h3></div>
                   <!-- /.card-header -->
                   <div class="card-body">
                     <table id="patientTable" class="table table-bordered table-striped">
