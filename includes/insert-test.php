f6b9a6fa2bded1b14176c70853e43e5375123f7a
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-18 14:32:10 +0530

    d

--- includes/insert-test.php
+++ includes/insert-test.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     mysqli_stmt_close($stmt);
     mysqli_close($con);
 
-    header('Location: add-test.php');
+    header('Location: ../add-test.php');
     exit();
 }
 ?>
