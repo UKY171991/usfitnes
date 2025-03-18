340c800bcc5cb26ca12b3e4812c6c9de48a2ce84
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-18 10:57:03 +0530

    Admin lte

--- inc/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Check if user_id is set in the session
-if (!isset($_SESSION['user_id'])) {
-    header("Location: index.php");
-    exit;
-}
-?>
--- inc/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Database Connection
-$servername = "localhost";
-$username = "u902379465_fitness";
-$password = "o#YIXdjR=1";
-$dbname = "u902379465_fitness";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
-?>
