eceec16287c50070b7c6c01ce426fe29b79211d4
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-18 11:31:13 +0530

    h

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -34,12 +34,6 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./generate/theme.html" class="nav-link">
-                  <i class="nav-icon bi bi-palette"></i>
-                  <p>Theme Generate</p>
-                </a>
-              </li>
-              <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="nav-icon bi bi-box-seam-fill"></i>
                   <p>
