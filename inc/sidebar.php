5818759bc6a4633e6b73d813701e06a7a5499b0d
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-18 11:47:58 +0530

    Update sidebar.php

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -508,33 +508,6 @@
                   <p>FAQ</p>
                 </a>
               </li>
-              <li class="nav-item">
-                <a href="./docs/license.html" class="nav-link">
-                  <i class="nav-icon bi bi-patch-check-fill"></i>
-                  <p>License</p>
-                </a>
-              </li>
-              <li class="nav-header">MULTI LEVEL EXAMPLE</li>
-              
-              <li class="nav-header">LABELS</li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="nav-icon bi bi-circle text-danger"></i>
-                  <p class="text">Important</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="nav-icon bi bi-circle text-warning"></i>
-                  <p>Warning</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="nav-icon bi bi-circle text-info"></i>
-                  <p>Informational</p>
-                </a>
-              </li>
             </ul>
             <!--end::Sidebar Menu-->
           </nav>
