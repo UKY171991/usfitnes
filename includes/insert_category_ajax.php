ce6de8cb0e4bb1006f3b09d8fe4cd3ad24c345a7
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-18 14:51:45 +0530

    insete test category

--- includes/insert_category_ajax.php
+++
@@ -1,1 +0,0 @@
-insert_category_ajax.php
--- includes/insert_test_category.php
+++ includes/insert_test_category.php
@@ -1,1 +1,19 @@
-insert_test_category.php
+<?php
+include('conn.php');
+
+if(isset($_POST['category_name'])){
+    $name = mysqli_real_escape_string($con, $_POST['category_name']);
+
+    $stmt = mysqli_prepare($con, "INSERT INTO test_categories (category_name) VALUES (?)");
+    mysqli_stmt_bind_param($stmt, 's', $name);
+
+    if(mysqli_stmt_execute($stmt)){
+        echo "success";
+    } else {
+        echo "error";
+    }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($con);
+}
+?>
