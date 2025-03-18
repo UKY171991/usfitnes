286141196ab321facd846e8c8c6b2c27204cc792
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-18 14:55:00 +0530

    Update js.php

--- inc/js.php
+++ inc/js.php
@@ -1,3 +1,6 @@
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js" integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ=" crossorigin="anonymous"></script>
     <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
     <script
