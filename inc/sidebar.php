60b0bb331aa3263cc7e4f8978953b5e87c1be4ba
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-24 17:16:45 +0530

    Update sidebar.php

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -66,13 +66,7 @@
                 </a>
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
-                    <a href="test-categories.php" class="nav-link">
-                      <i class="nav-icon bi bi-circle"></i>
-                      <p>Test Categories</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="add-test.php" class="nav-link">
+                    <a href="add_test.php" class="nav-link">
                       <i class="nav-icon bi bi-circle"></i>
                       <p>Add Test</p>
                     </a>
