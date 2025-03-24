bcb1baf800c9dca71c6fd679c1c58e9bed6e9ff7
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-24 16:46:28 +0530

    Update patients.php

--- patients.php
+++ patients.php
@@ -120,7 +120,7 @@ if (isset($_GET['delete']) && $_SESSION['role'] === 'Admin') {
                                     <td>${patient.first_name} ${patient.last_name}</td>
                                     <td>${patient.dob}</td>
                                     <td>${patient.gender}</td>
-                                    <td>${patient.contact_number || '-'}</td>
+                                    <td>${patient.phone || '-'}</td>
                                     <td>${patient.email || '-'}</td>
                                     <td>${patient.created_by}</td>
                                     <td>
