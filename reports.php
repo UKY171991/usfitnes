eca15918ba77b4dac423fa6f7f5cfe9591f3044a
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-25 11:01:26 +0530

    Update reports.php

--- reports.php
+++ reports.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 require_once 'db_connect.php';
 require_once 'vendor/tcpdf/tcpdf.php'; // Adjust path as needed
 session_start();
