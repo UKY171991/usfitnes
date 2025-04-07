f37dd7e0d6e8368bb2c80c9bcec621b45d202c8d
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-07 11:14:48 +0530

    Update reports.php

--- reports.php
+++ reports.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'db_connect.php';
+require_once 'db_connect.php'; 
 
 // Check if TCPDF is available
 if (!file_exists('vendor/tcpdf/tcpdf.php')) {
