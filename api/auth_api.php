115b276e6e34e77aeadc76440c82a70f4cbe6c4b
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-06-30 08:02:23 +0530

    Update auth_api.php

--- api/auth_api.php
+++ api/auth_api.php
@@ -72,7 +72,8 @@ function handleLogin($pdo, $input) {
         $stmt->execute([$username]);
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
         
-        if ($user && password_verify($password, $user['password'])) {            // Set session variables
+        if ($user && password_verify($password, $user['password'])) {
+            // Set session variables
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['username'] = $user['username'];
             $_SESSION['full_name'] = $user['full_name'];
